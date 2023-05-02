2610eb17eca001f67864b0bf36d0d47d39ae0555
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-05-02 19:50:34 -0400

    updating hw11

--- HW11/src/edu/umb/cs681/hw11/AccessCounter.java
+++ HW11/src/edu/umb/cs681/hw11/AccessCounter.java
@@ -1,22 +1,25 @@
 package edu.umb.cs681.hw11;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class AccessCounter {
 	private static AccessCounter instance = null;
 	private HashMap<Path, Integer> accessCountMap = new HashMap<>();
-	private static ReentrantLock lock = new ReentrantLock();
+	private static ReentrantLock slock = new ReentrantLock();
+	private ReentrantLock lock = new ReentrantLock();
 	
 	public static AccessCounter getInstance() {
-		lock.lock();
+		slock.lock();
 		try {
 		if (instance == null)
 			instance = new AccessCounter();
 			return instance;
 		} finally {
-			lock.unlock();
+			slock.unlock();
 		}
     }
 	
@@ -50,22 +53,33 @@ public class AccessCounter {
 	}
 	
 	public static void main(String[] args) {
-		 for(int i = 0; i < 10; i++) {
-			 RequestHandler handler = new RequestHandler();
-		     Thread thread = new Thread(handler);
-		     thread.start();
-		     try {
-				 Thread.sleep(1000);
-			 } catch (InterruptedException e) {
-				 e.printStackTrace(); 
-			 }
-		     handler.setdone();
-		     try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		 }
-		 System.out.println("Done");
+		List<Thread> threads = new ArrayList<>();
+		List<RequestHandler> handlers = new ArrayList<>();
+		for(int i=0;i<10;i++) {
+			RequestHandler handler = new RequestHandler();
+			Thread thread = new Thread(handler);
+			thread.start();
+			threads.add(thread);
+			handlers.add(handler);
+		}
+	    try {
+			Thread.sleep(1000);
+		} catch (InterruptedException exception) {
+			exception.printStackTrace();
+		}
+	    for (RequestHandler handler : handlers) {
+	    	handler.setdone();
+	    }
+	    for (Thread thread : threads) {
+	    	thread.interrupt();
+	    }
+	    for (Thread thread : threads) {
+	    	try {
+	    		thread.join();
+	    	} catch (InterruptedException e) {
+	    		e.printStackTrace();
+	    	}
+	    }
+	    System.out.println("Done");
 	}
 }
